60e138f962c418ed864eb09a43a50ced228aa39c
ishraklancer <ishraklancer@gmail.com>
2025-02-01 01:34:41 +0600

    Add main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,30 @@
 // filepath: /d:/code/Web/aora-one/aora/app/index.tsx
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import React from "react";
 import { Link } from "expo-router"; // Import the global.css file in the index.js file:
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images} from '../constants'
 
 const index = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-4xl font-pblack">Hello World!</Text>
-      <Link href="/home" className="text-blue-500">
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="h-full">
+        <View className="w-full justify-center items-center h-full px-4"> 
+          <Image
+            source={images.logo} 
+            className="w-[130px] [h-130px]"
+            resizeMode="contain"
+          />
+          
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
